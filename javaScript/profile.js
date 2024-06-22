e0b83ff74f0371d944c422ea14d498cd9b2735fa
paward661 <patsoccer125@gmail.com>
2024-06-22 16:27:27 -0700

    Profile Pic Update
    - Throws away pic if the user cancels the edit

--- javaScript/profile.js
+++ javaScript/profile.js
@@ -1,4 +1,4 @@
-// Get references
+// Get references to html elements
 const editButton = document.getElementById('edit-button');
 const editButtonText = document.getElementById('edit-button-text');
 const editProfilePicButton = document.getElementById('edit-profile-pic');
@@ -13,11 +13,21 @@ const locInput = document.getElementById('text-input-loc');
 const emailText = document.getElementById('text-email');
 const phoneText = document.getElementById('text-phone');
 const locText = document.getElementById('text-loc');
-var imageSrc = document.getElementById('profile-pic').src
+var imageSrc = document.getElementById('profile-pic').src;
+var uploadedImageSrc = imageSrc;
 
 // Need to edit function so that it throws away image if cancel is selected
 // function editOn(cancelEdit = true) {
-function editOn() {
+// Edit the profile information
+function editOn(cancelEdit) {
+    // Unless editOn() used in save function, cancelEdit assumed to be true
+    cancelEdit = typeof cancelEdit !== 'undefined' ? cancelEdit : true;
+
+    if (cancelEdit && editButtonText.textContent == "[Cancel]") {
+        // Restore to original image
+        document.getElementById('profile-pic').src = imageSrc;
+    }
+
     // Toggle the 'hidden' class
     editProfilePicButton.classList.toggle('hidden');
     profilePicOverlay.classList.toggle('hidden');
@@ -28,60 +38,62 @@ function editOn() {
     locInput.classList.toggle('hidden');
 
     if (bioTextInput.classList.contains('hidden')) {
+        // Return to appropriate format when profile page is not being editted
         document.documentElement.style.setProperty('--profile-info-height', `23%`);
         editButtonText.textContent = "[Edit Profile]";
     } else {
         // Retrieve the current value of the CSS variable --profile-info-height
         const currentHeight = getComputedStyle(document.documentElement).getPropertyValue('--profile-info-height');
 
-        // Parse the numeric portion of the value (removing 'px' if present)
+        // Parse the numeric portion of the value
         const numericHeightPercentage = parseFloat(currentHeight);
 
         // Add 80px to the numeric value
         const viewportHeight = window.innerHeight;
         const newHeight = viewportHeight * (numericHeightPercentage / 100) + 80;
-        // const newHeight = 80;
 
         // Update the CSS variable with the new value
         document.documentElement.style.setProperty('--profile-info-height', `${newHeight}px`);
         editButtonText.textContent = "[Cancel]";
     }
-
-    // if (cancelEdit && editButtonText.textContent == "[Cancel]") {
-
-    // }
 }
 
+// Allows a user to select an image locally.
 function uploadImage() {
     var selectedFile = this.files[0];
     if (selectedFile) {
         var reader = new FileReader();
+        // Displays profile pic after an image is selected
         reader.onload = function(event) {
-            var uploadedImageSrc = event.target.result;
-
-            fetch('https://rc8a0osdn1.execute-api.us-west-1.amazonaws.com/v1/profile-pic', {
-                method: 'POST',
-                body: uploadedImageSrc, // Pass the base64-encoded image data
-                headers: {
-                    'Content-Type': 'image/jpeg', // Set the content type appropriately
-                },
-            })
-            .then(response => {
-                if (response.ok) {
-                    // Image uploaded successfully
-                    document.getElementById('profile-pic').src = uploadedImageSrc;
-                } else {
-                    console.error('Image upload failed:', response.statusText);
-                }
-            })
-            .catch(error => {
-                console.error('Error uploading image:', error);
-            });
+            uploadedImageSrc = event.target.result;
+            document.getElementById('profile-pic').src = uploadedImageSrc;
         };
         reader.readAsDataURL(selectedFile);
     }
 }
 
+function saveImageToS3() {
+    fetch('https://rc8a0osdn1.execute-api.us-west-1.amazonaws.com/v1/profile-pic', {
+            method: 'POST',
+            body: uploadedImageSrc, // Pass the base64-encoded image data
+            headers: {
+                'Content-Type': 'image/jpeg',
+            },
+        })
+        .then(response => {
+            if (response.ok) {
+                // Image uploaded successfully
+                imageSrc = uploadedImageSrc
+            } else {
+                console.error('Image upload failed:', response.statusText);
+            }
+        })
+        .catch(error => {
+            console.error('Error uploading image:', error);
+        });
+}
+
+// Save the edits to the profile information
 function save() {
     const defaultBioText = "Enter text here";
     const preBioText = "Bio: ";
@@ -101,18 +113,22 @@ function save() {
         }
         input.value = defaultText;
     }
-    editOn();
+    saveImageToS3();
+    editOn(false);
 }
 
-// Add a click event listener to the button
+// Add a click event listener to the edit button
 editButton.addEventListener('click', editOn);
 
+// Add a click event listener to the edit profile pic button
 editProfilePicButton.addEventListener('click', function(event) {
     event.preventDefault();
     fileInput.click();
 });
 
+// If user provides an image, display it
 fileInput.addEventListener('change', uploadImage);
 
+// Saves provided profile info if user clicks save button
 saveEditsButton.addEventListener('click', save);
 
