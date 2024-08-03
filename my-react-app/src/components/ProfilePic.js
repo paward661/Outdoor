c2f087bbf652884606b574030e6257b612787051
paward661 <patsoccer125@gmail.com>
2024-08-03 09:57:55 -0700

    Added React Project
    Starting to learn react

---
+++ my-react-app/src/components/ProfilePic.js
@@ -0,0 +1,12 @@
+function ProfilePic() {
+  return (
+    <div className="circular-photo">
+      <img
+        src="../../../images/beach heasdshot.JPG"
+        alt="The user's profile picture"
+      />
+    </div>
+  );
+}
+
+export default ProfilePic;
