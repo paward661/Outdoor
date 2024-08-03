9a1dae4d933dcbbbd530ea5d77fb67f95b8f1575
paward661 <patsoccer125@gmail.com>
2024-08-03 14:14:00 -0700

    Feed placeholder
    Added Feed placeholder

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 // import './App.css';
 import Header from "./components/Header";
+import Feed from "./components/Feed";
 
 function App() {
   return (
     <div>
       <Header />
+      <Feed />
     </div>
   );
   // return (
---
+++ my-react-app/src/components/Feed.js
@@ -0,0 +1,14 @@
+import "./Feed.css";
+
+function Feed() {
+  return (
+    <div className="feed">
+      <div id="my-feed">
+        {/* The live feed content will be displayed here */}
+        <h2>Live Feed</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Feed;
